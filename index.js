d4ae68f865d8bfedc5cbf0ac80bb45cf1859db13
Sheza25 <shezakareem025@gmail.com>
2026-04-28 03:42:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
-const video = document.querySelector("video");
+let video = document.getElementById("video");
+let recordedVideo = document.getElementById("recordedVideo");
 
-navigator.mediaDevices.getUserMedia({ video: true, audio:true })
-  .then(stream => {
-    video.srcObject = stream;
-  })
-  .catch(err => {
-    console.log("Camera access denied:", err);
-  });
+let mediaStream;
+let mediaRecorder;
+let chunks = [];
+
+// Start camera + mic
+async function startMedia() {
+  try {
+    mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    });
+
+    video.srcObject = mediaStream;
+    console.log("Camera & Mic started");
+
+  } catch (err) {
+    console.error("Permission denied:", err);
+  }
+}
+
+// Start recording
+function startRecording() {
+  if (!mediaStream) {
+    alert("Start camera first!");
+    return;
+  }
+
+  chunks = [];
+  mediaRecorder = new MediaRecorder(mediaStream);
+
+  mediaRecorder.ondataavailable = e => {
+    chunks.push(e.data);
+  };
+
+  mediaRecorder.onstop = () => {
+    const blob = new Blob(chunks, { type: "video/webm" });
+    const videoURL = URL.createObjectURL(blob);
+    recordedVideo.src = videoURL;
+  };
+
+  mediaRecorder.start();
+  console.log("Recording started");
+}
+
+// Stop recording
+function stopRecording() {
+  mediaRecorder.stop();
+  console.log("Recording stopped");
+}
